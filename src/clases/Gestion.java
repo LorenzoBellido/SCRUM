03e56feabaaf270af8aad372cf8b0fb5ad3aef0a
ljbellido <ljbellido@106-24.info2>
2024-05-14 12:11:39 +0200

    Lorenzo

--- src/clases/Gestion.java
+++ src/clases/Gestion.java
@@ -6,6 +6,27 @@ public class Gestion {
 
 	private HashSet<Viaje> viajes = new HashSet<Viaje>();
 
+	public Gestion() {
+	}
+
+	public boolean anyadirViaje(Viaje v) {
+		boolean anadido = false;
+		if(v != null) {
+			viajes.add(v);
+			anadido = true;
+		}
+		return anadido;
+	}
+	
+	public boolean borrarViaje(Viaje v) {
+		boolean borrado = false;
+		if(viajes.contains(v)) {
+			viajes.remove(v);
+			
+			borrado = true;
+		}
+		return borrado;
+	}
 	
 	
 	
