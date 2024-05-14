6816f5873cf2312ae39f28e8e8911dbd2b2eb80d
raulromerap <raul.romera@iesnervion.com>
2024-05-14 12:40:43 +0200

    14/05/2024

--- src/clases/IOClass.java
+++ src/clases/IOClass.java
@@ -1,6 +1,28 @@
 package clases;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class IOClass {
 
+	public static BufferedReader reader = null;
+	public static BufferedWriter writer = null;
+	public static String linea ="";
+	
+	
+	static public boolean leer() {
+		boolean ret = false;
+			
+			try {
+				if((linea = reader.readLine())!=null) {
+					ret = true;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return ret;
+	}
 	
 }
