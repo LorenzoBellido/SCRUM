52d3020e26915492c901fa919f69e06071b4b7fb
ljbellido <ljbellido@106-24.info2>
2024-05-16 13:14:28 +0200

    Cookeada de Lorenzo🔥🔥🔥

--- src/clases/Gestion.java
+++ src/clases/Gestion.java
@@ -1,13 +1,18 @@
 package clases;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Gestion {
 
 	static public HashSet<Viaje> viajes = new HashSet<Viaje>();
+	
+	static public ArrayList<Viaje> viajesBusqueda= new ArrayList<Viaje>();
 
 	public Gestion() {
 	}
+	
+	
 
 	public boolean anyadirViaje(Viaje v) {
 		boolean anadido = false;
@@ -18,13 +23,14 @@ public class Gestion {
 		return anadido;
 	}
 	
-	public boolean borrarViaje(Viaje v) {
+	public boolean borrarViaje(int opc) {
 		boolean borrado = false;
-		if(viajes.contains(v)) {
-			viajes.remove(v);
+		Viaje v = null;
+		if(opc >= 0) {
+			v = viajesBusqueda.get(opc);
 			
-			borrado = true;
-		}
+			borrado = viajes.remove(v);
+			}
 		return borrado;
 	}
 	
@@ -32,4 +38,15 @@ public class Gestion {
 		System.out.println(viajes);
 	}
 	
+	public static boolean buscar(String lugar) {
+		boolean encontrado = false;
+		int cont = 0;
+		for(Viaje v : viajes) {
+			if(lugar.equalsIgnoreCase(v.getLugar())) {
+				encontrado = true;
+				viajesBusqueda.add(v);
+			}
+		}
+		return encontrado;
+	}
 }
